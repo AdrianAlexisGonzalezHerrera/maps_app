a1c0e706b2f15b367ae3194c59f41b037f129d2b
Adrian González <adrianalexisgonzalezherrera@gmail.com>
2023-11-02 14:02:22 +0100

    MapsApp - Fin De La Sección 17

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:maps_app/blocs/custom_blocs.dart';
+
+import 'package:maps_app/screens/custom_screens.dart';
+
+void main() {
+  // runApp(const MapsApp());
+  runApp(MultiBlocProvider(
+    providers: [BlocProvider(create: (context) => GpsBloc())],
+    child: const MapsApp(),
+  ));
+}
+
+class MapsApp extends StatelessWidget {
+  const MapsApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'MapsApp',
+        home: LoadingScreen());
+  }
+}
